16d0beb59dcb513266ea92907ea8c7995a11fd6e
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-21 14:39:01 +0100

    refactor: update ControllerAdvice to RestControllerAdvice

--- src/main/java/com/tomatopay/transactionservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tomatopay/transactionservice/exception/GlobalExceptionHandler.java
@@ -11,8 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({TransactionNotFoundException.class})
