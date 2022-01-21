2515bcc9e21e612cecf2aefc94e568208e962778
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-21 17:26:10 +0100

    refactor: update schema script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS account (
+    id VARCHAR(19) PRIMARY KEY,
+    balance DECIMAL(12,2) DEFAULT 0.00 NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS transaction (
     id VARCHAR(19) PRIMARY KEY,
     account_id VARCHAR(19),
@@ -6,10 +11,5 @@ CREATE TABLE IF NOT EXISTS transaction (
     description TEXT,
     type VARCHAR(5),
     FOREIGN KEY (account_id) REFERENCES account(id)
-);
-
-CREATE TABLE IF NOT EXISTS account (
-    id VARCHAR(19) PRIMARY KEY,
-    balance DECIMAL(12,2) DEFAULT 0.00 NOT NULL
 );
 
