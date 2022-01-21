98a902658dcc8f175e0b83fa01e1a459c23bbd76
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-21 17:28:36 +0100

    refactor: update return type of TransactionService interface methods

--- src/main/java/com/tomatopay/transactionservice/service/TransactionService.java
+++ src/main/java/com/tomatopay/transactionservice/service/TransactionService.java
@@ -4,13 +4,18 @@ import com.tomatopay.transactionservice.dto.request.TransactionRequest;
 import com.tomatopay.transactionservice.dto.response.TransactionResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface TransactionService {
 
     TransactionResponse createTransaction(TransactionRequest transactionRequest);
 
-    List<TransactionResponse>  getTransactions();
+    TransactionResponse updateTransaction(TransactionRequest transactionRequest);
 
+    TransactionResponse deleteTransaction(UUID id);
 
+    TransactionResponse getTransaction(UUID id);
+
+    List<TransactionResponse>  getTransactions(Integer page, Integer pageSize);
 
 }
