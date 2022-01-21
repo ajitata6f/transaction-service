e78ae87834028dbde7106248a97402c962a8b9eb
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-21 16:40:21 +0100

    feat: add TransactionRequest dto class

---
+++ src/main/java/com/tomatopay/transactionservice/dto/request/TransactionRequest.java
@@ -0,0 +1,53 @@
+package com.tomatopay.transactionservice.dto.request;
+
+import java.math.BigDecimal;
+
+public class TransactionRequest {
+
+    private String accountId;
+    private String currency;
+    private BigDecimal amount;
+    private String description;
+    private String transactionType;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+}
