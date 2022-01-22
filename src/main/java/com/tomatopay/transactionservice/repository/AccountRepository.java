8cacb3c942d17fac51bf88ecc45545bb084a9d2c
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-22 15:40:16 +0100

    fix: update id type to String

--- src/main/java/com/tomatopay/transactionservice/repository/AccountRepository.java
+++ src/main/java/com/tomatopay/transactionservice/repository/AccountRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends PagingAndSortingRepository<Account, Integer> {
+public interface AccountRepository extends PagingAndSortingRepository<Account, String> {
 
 }
--- src/main/java/com/tomatopay/transactionservice/repository/TransactionRepository.java
+++ src/main/java/com/tomatopay/transactionservice/repository/TransactionRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Integer> {
+public interface TransactionRepository extends PagingAndSortingRepository<Transaction, String> {
 
 }
