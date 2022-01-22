a11bba83f35a6da7043811b787e6bb2e1e378aaa
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-22 15:40:48 +0100

    refactor: update schema DDL script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS account (
-   id serial,
+   id CHARACTER VARYING(36),
    balance DECIMAL(12,2) DEFAULT 0.00 NOT NULL,
    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
-    id serial,
-    account_id integer,
-    currency VARCHAR(5),
-    amount DECIMAL(12,2) NOT NULL,
-    description TEXT,
-    type VARCHAR(5),
-    PRIMARY KEY (id),
-    FOREIGN KEY (account_id) REFERENCES account(id)
+   id CHARACTER VARYING(36),
+   account_id CHARACTER VARYING(36),
+   currency VARCHAR(5),
+   amount DECIMAL(12,2) NOT NULL,
+   description TEXT,
+   type VARCHAR(6),
+   PRIMARY KEY (id),
+   FOREIGN KEY (account_id) REFERENCES account(id)
 );
