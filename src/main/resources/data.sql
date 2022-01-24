411e6839c0514f61ecf03d4da161da2d889fdb69
adamu abdulaziz <ajitata6f@gmail.com>
2022-01-24 09:51:29 +0100

    build: update data.sql DB initialization script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO account(balance) VALUES(1000.00);
-INSERT INTO account(balance) VALUES(2000.00);
-INSERT INTO account(balance) VALUES(3000.00);
+INSERT INTO account(id, balance) VALUES('a5814c9a-20fc-4967-b272-83234be457d6', 1000.00);
+INSERT INTO account(id, balance) VALUES('a5814c9a-20fc-4967-b272-83234be457ty', 2000.00);
+INSERT INTO account(id, balance) VALUES('a5814c9a-20gf-4967-b272-83234be457d6', 3000.00);
